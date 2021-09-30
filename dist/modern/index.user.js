67c99292d3ee6d3d34a3ed79e19f9a970456f6a1
Oleg Valter <o.a.philippov@gmail.com>
2021-09-30 18:48:56 +0300

    0.2.2

--- dist/modern/index.user.js
+++ dist/modern/index.user.js
@@ -3,18 +3,24 @@
 // @description     adds a garbage bin icon to custom filters
 // @grant           none
 // @homepage        https://github.com/userscripters/garbage-filters#readme
+// @match           https://*.askubuntu.com/questions
 // @match           https://*.askubuntu.com/questions/*
+// @match           https://*.mathoverflow.net/questions
 // @match           https://*.mathoverflow.net/questions/*
+// @match           https://*.serverfault.com/questions
 // @match           https://*.serverfault.com/questions/*
+// @match           https://*.stackapps.com/questions
 // @match           https://*.stackapps.com/questions/*
+// @match           https://*.stackexchange.com/questions
 // @match           https://*.stackexchange.com/questions/*
+// @match           https://*.stackoverflow.com/questions
 // @match           https://*.stackoverflow.com/questions/*
 // @name            Garbage Filters
 // @namespace       userscripters
 // @run-at          document-start
 // @source          git+https://github.com/userscripters/garbage-filters.git
 // @supportURL      https://github.com/userscripters/garbage-filters/issues
-// @version         0.2.1
+// @version         0.2.2
 // ==/UserScript==
 
 "use strict";
